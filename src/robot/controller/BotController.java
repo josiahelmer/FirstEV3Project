405232f228fcf96ec4cfb95f8081a0863b2a2220
josiahelmer <jelm9149@740s-pl-112.local>
2015-12-10 13:55:34 -0700

    Bot Controller

--- src/robot/controller/BotController.java
+++ src/robot/controller/BotController.java
@@ -1,6 +1,36 @@
 package robot.controller;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+import robot.model.EV3Bot;
+
+
 public class BotController
 {
-
+	private String message;
+	
+	private int xPosition;
+	private int yPosition;
+	
+	private long waitTime;
+	
+	private EV3Bot JosiahBot;
+	
+	public BotController()
+	{
+		this.message = "Programming Lego robots with Java";
+		this.xPosition = 10;
+		this.yPosition = 10;
+		this.waitTime = 4000;
+		
+		JosiahBot = new EV3Bot();
+	}
+	
+	public void start()
+	{
+		LCD.drawString(message, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+		
+		JosiahBot.driveRoom();
+	}
 }
