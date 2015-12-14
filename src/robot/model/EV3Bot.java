dfc4b3c517b8c5ee0b9a3b33856387b49c488bd6
josiahelmer <jelm9149@740s-pl-112.local>
2015-12-14 13:53:22 -0700

    first driving commands

--- src/robot/model/EV3Bot.java
+++ src/robot/model/EV3Bot.java
@@ -1,6 +1,50 @@
 package robot.model;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+import robot.controller.BotController;
+
 public class EV3Bot
 {
+private String botMessage;
+private int xPosition;
+private int yPosition;
+private long waitTime;
+
+public EV3Bot()
+{
+
+	this.botMessage = "";
+	this.xPosition = 50;
+	this.yPosition = 50;
+	this.waitTime = 4000;
+}
+
+public void driveRoom()
+{
+	//call private helper method here
+}
+
+private void displayMessage()
+{
+
+	
+	//overload the displayyMessage method with a single String parameter
+	
+	//write your first drive method here
+	//call the displayMessage("describe action as a string") in the helper method
+	
+	LCD.drawString(botMessage, xPosition, yPosition);
+	Delay.msDelay(waitTime);
+	}
+	
+public void forward()
+{
+	Motor.A.forward();
+	Motor.B.forward();
+	Motor.A.Speed(720);// 2 RPM
+	Motor.B.Speed(720);// 2 RPM
+}
+
 
 }
