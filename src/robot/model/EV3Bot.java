28dc654c69e537c86523156ab23a9884b8134d19
josiahelmer <jelm9149@740s-pl-112.local>
2015-12-16 12:47:14 -0700

    fixed motor error

--- src/robot/model/EV3Bot.java
+++ src/robot/model/EV3Bot.java
@@ -1,6 +1,7 @@
 package robot.model;
 
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 import robot.controller.BotController;
 
@@ -18,33 +19,39 @@ public EV3Bot()
 	this.xPosition = 50;
 	this.yPosition = 50;
 	this.waitTime = 4000;
+	
+	displayMessage();
 }
 
-public void driveRoom()
-{
-	//call private helper method here
-}
-
-private void displayMessage()
-{
-
-	
-	//overload the displayyMessage method with a single String parameter
-	
-	//write your first drive method here
-	//call the displayMessage("describe action as a string") in the helper method
-	
-	LCD.drawString(botMessage, xPosition, yPosition);
-	Delay.msDelay(waitTime);
+	public void driveRoom()
+	{
+		//call private helper method here
+		displayMessage("driveRoom");
 	}
+
+	private void displayMessage()
+	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+
+//overload the displayyMessage method with a single String parameter
+	private void displayMessage(String mesage)
+	{
+		LCD.drawString(botMessage,  xPosition,  yPosition);
+		Delay.msDelay(waitTime);
+	}
+
+//write your first drive method here
+//call the displayMessage("describe action as a string") in the helper method
 	
-public void forward()
-{
+	public void forward()
+	{
 	Motor.A.forward();
 	Motor.B.forward();
-	Motor.A.Speed(720);// 2 RPM
-	Motor.B.Speed(720);// 2 RPM
-}
+	Motor.A.setSpeed(720);// 2 RPM
+	Motor.B.setSpeed(720);// 2 RPM
+	}
 
 
 }
